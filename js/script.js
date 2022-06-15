d2a5930597b1ca81ff588be4522804214edf0e5d
Enzo Degraeve <enzodeg40@gmail.com>
2022-06-15 21:18:34 +0200

    Possibilité de sauvegarder les bornes découvertes

--- js/script.js
+++ js/script.js
@@ -1,27 +1,87 @@
+// Ouvre l'image en plein écran lors du clic sur l'image
 function ouvre(num){
     window.open("bornes/"+bornes[num].nom, '_blank').focus();
+}
 
+// Lorsque qu'une borne à été découverte
+function definitDecouverteBorne(num, decouverte){
+    // Change l'icone du marker
+    if(decouverte == true){
+        makers[num].setIcon(borneDecouverte);
+    }
+    else{
+        makers[num].setIcon(borneNonDecouverte);
+    }
 
+    // Sauvegarde l'id de la borne dans la variable local
+    localStorage.setItem("borne" + num, decouverte); 
+}
 
+function updateBorne(){
+    // Pour chaque borne
+    for(var i = 0; i < bornes.length; i++){
+
+        if(localStorage.getItem("borne" + i) == 'true'){
+            makers[i].setIcon(borneDecouverte);
+        }
+        // Sinon
+        else{
+            makers[i].setIcon(borneNonDecouverte);
+        }
+    }
 }
 
 var map = L.map('map').setView([47.0016, 2.8], 6.4);
 // Définit la carte utilisé (openstreetmap)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution:'<a href="https://tinder.com/@enzodeg40">Enzo Degraeve</a> | <a href="https://pierron.iiens.net"> Hugo Pierron </a>'}).addTo(map);
 
+var borneNonDecouverte = L.icon({
+    iconUrl: 'lib/leaflet/images/marker-icon.png',
+    iconRetinaUrl: 'lib/leaflet/images/marker-icon-2x.png',
+    shadowUrl: 'lib/leaflet/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    tooltipAnchor: [16, -28],
+    shadowSize: [41, 41]
+});
 
+var borneDecouverte = L.icon({
+    iconUrl: 'lib/leaflet/images/marker-icon-green.png',
+    iconRetinaUrl: 'lib/leaflet/images/marker-icon-green-2x.png',
+    shadowUrl: 'lib/leaflet/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    tooltipAnchor: [16, -28],
+    shadowSize: [41, 41]
+});
 
+var makers = [];
 
 var i = 0
 bornes.forEach(borne => {
     if (borne.y!=0){
-        var marker = L.marker([(borne.x==0) ? i : borne.x, borne.y]).addTo(map).bindPopup(`
+        makers[i] = L.marker([(borne.x == 0) ? i : borne.x, borne.y], { icon: borneNonDecouverte })
+        .addTo(map).bindPopup(`
             <img onclick="ouvre(${i})" src="bornes/${borne.nom}" alt ="${borne.nom.replace(".jpg","")}" ><br/>
             <p class="nom">${borne.nom.replace(".jpg","")}</p>
-            <p>Coordonnées :</p>
-            <p>x : ${Math.round(1000*borne.x)/1000}, y: ${Math.round(1000*borne.y)/1000}</p>
-            <p> Altitude : ${(borne.alt=="inconnue") ? borne.alt : (borne.alt+"m")}</p>
-            <a href="https://www.google.com/maps/search/?api=1&query=${borne.x}, ${borne.y}" target="_blank">Google maps</a>
+            <p>x: ${Math.round(1000*borne.x)/1000} y: ${Math.round(1000*borne.y)/1000}</p>
+            <p>${(borne.alt=="inconnue") ? borne.alt : (borne.alt+" mètres")}</p>
+            
+            <div class="options">
+                <a class="option" href="https://www.google.com/maps/search/?api=1&query=${borne.x}, ${borne.y}" target="_blank">
+                    <img src="img/google_maps.svg" alt="Google maps">
+                </a>
+                
+                <div class="option" onclick="definitDecouverteBorne(${i}, false)">
+                    <img src="img/none.svg" alt="Non découverte">
+                </div>
+
+                <div class="option" onclick="definitDecouverteBorne(${i}, true)">
+                    <img src="img/done.svg" alt="Découverte">
+                </div>
+            </div>
         `);
 
         document.getElementById('bornelist').innerHTML += `<option value="${borne.nom.replace(".jpg","")}">`;
@@ -46,3 +106,5 @@ inputlist.addEventListener('change', (event) => {
     }
     });
 });
+
+updateBorne();
