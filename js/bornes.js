2453100ebb7c3b74d05fa68adfda9aabfe02c078
Enzo Degraeve <enzodeg40@gmail.com>
2022-09-03 18:39:48 +0200

    Ajout bornes Crisenoy

--- js/bornes.js
+++ js/bornes.js
@@ -231,7 +231,7 @@ var bornes = [
     {"id":227, "nom": "Porte de Paris Lille", "x": "50.629724509925", "y": "3.0688286895351746", "alt": "22,53" },
     {"id":228, "nom": "Théâtre du nord Lille", "x": "50.636437323164515", "y": "3.063837341501822", "alt": "20.980" },
     {"id":229, "nom": "Rue du Lavoir de Maincy", "x": "48.55003597482087", "y": "2.701338910654465", "alt": "83.45" },
-    {"id":230, "nom": "Hôtel de ville Bruxelles", "x": "50.84616263176381", "y": "4.351697999709192", "alt": "21.15" },
+    { "id": 230, "nom": "Hôtel de ville Bruxelles", "x": "50.84616263176381", "y": "4.351697999709192", "alt": "21.15", "ville": "Bruxelles", "wiki":"https://fr.wikipedia.org/wiki/H%C3%B4tel_de_ville_de_Bruxelles" },
     {"id":231, "nom": "Office du tourisme Hyères", "x": "43.11992027141953", "y": "6.130933392790056", "alt": "inconnue" },
     {"id":232, "nom": "Porte Massillon Hyères", "x": "43.12072141596795", "y": "6.130726357376559", "alt": "inconnue" },
     {"id":233, "nom": "Hôtel de Ville Hyères", "x": "43.11999823353953", "y": "6.130301758195966", "alt": "inconnue" },
@@ -242,7 +242,6 @@ var bornes = [
     { "id": 238, "nom": "Chapelle Sainte-Reine de Châtelet-en-Brie", "x": "48.50246242201866", "y": "2.794911003592093", "alt": "87", "ville": "Le Châtelet-en-Brie" },
     { "id": 239, "nom": "Pont du Ru de Chatelet", "x": "48.50283856424969", "y": "2.7942852805504126", "alt": "inconnue", "ville": "Le Châtelet-en-Brie" },
     { "id": 240, "nom": "Église Saint-Étienne de Maincy", "x": "48.550427597113874", "y": "2.699498827856823", "alt": "inconnue", "ville": "Maincy" },
-
     { "id": 241, "nom": "Ferme lieu dit Pointe Pierre", "x": "45.81653735846607", "y": "2.0305193126940964", "alt": "738", "ville": "Gentilloux Pigerolles" },
     { "id": 242, "nom": "Buse sortie Pointe Pierre", "x": "45.81775334271881", "y": "2.0327293917973024", "alt": "752", "ville": "Gentilloux Pigerolles" },
     { "id": 243, "nom": "Pilier en Caillou croisement D992 et D26 La Nouaille", "x": "45.81904285573936", "y": "2.0417334233233095", "alt": "772.509", "ville": "La Nouaille" },
@@ -253,4 +252,7 @@ var bornes = [
     { "id": 248, "nom": "Lieu dit le Poteau de Maussac", "x": "45.467609765860864", "y": "2.143525120077443", "alt": "600", "ville": "Maussac" },
     { "id": 249, "nom": "Rue de la Forge Millevache", "x": "45.641032464610774", "y": "2.095344099997953", "alt": "907", "ville": "Millevache" },
     { "id": 250, "nom": "Rue principale Millevache", "x": "45.64177408503842", "y": "2.0948910842409156", "alt": "911", "ville": "Millevache" },
+    { "id": 251, "nom": "Rue de l'église de Crisenoy", "x": "48.59641611523292", "y": "2.7448527698304614", "alt": "90", "ville": "Crisenoy" },
+    { "id": 252, "nom": "Ru d'Andy", "x": "48.59355495603607", "y": "2.7488117979778974", "alt": "88", "ville": "Crisenoy" },
+    { "id": 253, "nom": "Mairie de Crisenoy", "x": "48.595664", "y": "2.740600", "alt": "inconnue", "ville": "Crisenoy" },
 ]
